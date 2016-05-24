3303cb16f703204874485603eb7b9a3fe488b2e6
Andrew Fang <andrewbrianfang@gmail.com>
2016-05-23 17:15:54 -0700

    init server code

---
+++ nodeServer.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var firebase = require('firebase');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
+// Writes asynchronously
+
+// Creates the app for express
+var app = express();
+
+// Enables json parsing
+app.use(bodyParser.json());
+
+// Initializing Firebase Backend
+var myDataRef = new Firebase('https://luminous-heat-8591.firebaseio.com/');
+
+// Server responding to GET request
+app.get('/notes', function (req,res) {
+    var returnData = [];
+	myDataRef.once("value", function(snap) {
+        snap.forEach(function(valueSnap) {
+            returnData.push({x:parseFloat(valueSnap.val().x), y:parseFloat(valueSnap.val().y), z:parseFloat(valueSnap.val().z), image:valueSnap.val().image});
+        })
+		//console.log("All data: ", snap.val());
+        console.log("Return data: ", returnData);
+        res.contentType('application/json');
+        res.send(JSON.stringify(returnData)); 
+	});
+
+	//res.sendFile(__dirname+ '/index.html');
+});
+
+
+// Server responding to POST request
+app.post('/notes', function (req,res) {
+
+	// Storing data into database
+
+    console.log("POST RECEIVED: ");
+    // console.log(req.param("x"));
+    // console.log(req.params.x);
+
+    var body = '';
+    // Event listener for starting to read data (save data here)
+    // Right now we only use these event listeners to debug
+    req.on('data', function (data) {
+        body += data;
+        // console.log("Partial body: " + body);
+        
+
+        // Parsing the data from the request (TODO: NEED TO USE REAL JSON ATTRIBUTES)
+        
+        // TODO: COMMENT THIS AFTER WE GET ACTUAL IMPLEMENTATION WORKING!
+        //myDataRef.push({requestText:body});
+
+        // TODO: UNCOMMENT THESE NEXT 5 LINES AFTER ACTUAL IMPLEMENTATION IS WOKRING
+        console.log()
+        var noteDict = {};
+        var splitData = body.split("&");
+
+        if (splitData[0] && splitData[1] && splitData[2] && splitData[3]) {
+
+            var first = splitData[0].split("=")
+            var second = splitData[1].split("=")
+            var third = splitData[2].split("=")
+            var fourth = splitData[3].split("=")
+
+            noteDict[first[0]] = first[1]
+            noteDict[second[0]] = second[1]
+            noteDict[third[0]] = third[1]
+            noteDict[fourth[0]] = fourth[1]
+
+            var x = noteDict["x"];
+            var y = noteDict["y"];
+            var z = noteDict["z"];
+            var image = noteDict["image"];
+
+            myDataRef.push({image:image, x:x, y:y, z:z});
+
+        } else {
+            console.log("invalid data format");
+        }
+
+    });
+
+    // Event listener for after end of data reached (perform actions to be done after data is processed here)
+    req.on('end', function () {
+        //console.log("Body: " + body);
+
+    });
+
+    // Writing to the response and sending it
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('post received');
+});
+
+// App listening on Port 3000
+app.listen(3000, function() {
+  console.log("Listening on Port 3000");
+});
